c791ff3c724e5b36a487e7c516f01c30240f1766
Vladimir <googolplex0@yandex.ru>
2016-11-08 23:39:14 +0300

    Adding new files

---
+++ config.php
@@ -0,0 +1,6 @@
+<?
+$host = "localhost";
+$user = "root";
+$password = "";
+$db = "topface";
+$mysqli = mysqli_connect($host, $user, $password, $db);
---
+++ index.php
@@ -0,0 +1,33 @@
+﻿<?php
+	
+?>
+<html>
+	<head>
+		<link rel="stylesheet" href="css/styles.css">
+	</head>
+	<body>
+		<div class="registration_form">
+			<form name="registration_form">
+				<div class="controls">
+					<label class="form_label" >Логин:</label>
+					<input type="text" name="login" maxlength="32"></input>
+				</div>
+				
+				<div class="controls">
+					<label class="form_label">Пароль:</label>
+					<input type="password" name="password" maxlength="32"></input>
+				</div>
+				
+				<div class="controls">
+					<label class="form_label">Повторите пароль:</label>
+					<input type="password" name="password2" maxlength="32"></input>
+				</div>
+				
+				<div class="controls">
+					<label class="form_label">Дата рождения:</label>
+					<input type="date" name="birthday" maxlength="32"></input>
+				</div>
+			</form>
+		</div>
+	</body>
+</html>
